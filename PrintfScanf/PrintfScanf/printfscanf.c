e8324b2f99a928d2359d17a35cd445780507f248
seoyeonida <rsy012@naver.com>
2022-07-11 18:27:11 +0900

    C언어 공부

---
+++ PrintfScanf/PrintfScanf/printfscanf.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main(void)
+{
+	// 정수형 변수에 대한 예제
+
+	/*
+	int age = 12;
+	printf("%d\n", age);
+
+	age = 13;
+	printf("%d\n", age);
+	return 0;
+	*/
+
+	// 실수형 변수에 대한 예제
+
+	/*
+	float f = 46.5f;
+	printf("%f\n", f);
+	return 0;
+	*/
+
+	/*
+	float f = 46.5f;
+	printf("%.2f\n", f); // .2 를 추가하여 소수점 둘째 자리까지만 출력
+	return 0;
+
+	double d = 4.428;
+	printf("%.2lf\n", d); // 소수점 둘째 자리까지만 출력하므로 반몰림됨
+	return 0;
+	*/
+
+	//상수
+
+	/*
+	const int YEAR = 2000; //const가 붙으면 상수가 되어 변하지 않게됨
+	printf("태어난 년도 : %d\n", YEAR);
+	// YEAR = 2001
+	return 0;
+	*/
+	
+}
